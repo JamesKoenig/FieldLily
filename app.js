dd596ef918105b8dcfa6d4188387cbf5afe0a6e8
Qi Zhu <rubybfm1017@gmail.com>
2021-02-22 15:19:13 -0800

    building models and routes

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const mongoose = require('mongoose');
 const app = express();
+const User = require('./models/User');
+const bodyParser = require('body-parser');
 
 const db = require('./config/keys').mongoURI;
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
+
+app.use(bodyParser.urlencoded({
+    extended:false
+}));
+
+app.use(bodyParser.json());
+
 
 app.get("/", (req, res) => res.send("Hello aa"));
 
