22eb558dbd64bce9361d7f5a3eb8f8589311a4a4
James Koenig <Koenig.James@Live.com>
2021-02-23 14:52:17 -0800

    add errors reducer to root reducer

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 import session from './session_reducer.js';
+import errors from './errors_reducer.js';
 
 const RootReducer = combineReducers({
+  errors,
   session
 });
 
