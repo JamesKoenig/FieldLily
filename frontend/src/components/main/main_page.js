2dcce9a2b2156c23b3840a706f680278a6e7a950
Itai Farhi <itaimf@gmail.com>
2021-02-23 13:24:00 -0800

    add main

---
+++ frontend/src/components/main/main_page.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class MainPage extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>Field Lily</h1>
+        <footer>
+        BSD 3 &copy; 2021
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default MainPage;
