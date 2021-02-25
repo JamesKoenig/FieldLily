8a00f3afe7cbf2f99c7e9e899a45d46b875911b5
James Koenig <Koenig.James@Live.com>
2021-02-25 14:20:14 -0800

    fix user auth on signup
    we need to know the current user regardless of whether they signed in or
    signed up so this diverges from mern_twitter's approach

--- frontend/src/actions/session_actions.js
+++ frontend/src/actions/session_actions.js
@@ -4,17 +4,12 @@ import jwt_decode from 'jwt-decode';
 export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
 export const RECEIVE_SESSION_ERRORS = "RECEIVE_SESSION_ERRORS";
 export const RECEIVE_USER_LOGOUT = "RECEIVE_USER_LOGOUT";
-export const RECEIVE_USER_SIGN_IN = "RECEIVE_USER_SIGN_IN";
 
 export const receiveCurrentUser = currentUser => ({
     type: RECEIVE_CURRENT_USER,
     currentUser
 });
 
-export const receiveUserSignIn = () => ({
-    type: RECEIVE_USER_SIGN_IN
-});
-  
 export const receiveErrors = errors => ({
     type: RECEIVE_SESSION_ERRORS,
     errors
@@ -25,8 +20,8 @@ export const logoutUser = () => ({
 });
 
 export const signup = user => dispatch => (
-    APIUtil.signup(user).then(() => (
-        dispatch(receiveUserSignIn())
+    APIUtil.signup(user).then(user => (
+        dispatch(receiveCurrentUser(user))
     ), err => (
         dispatch(receiveErrors(err.response.data))
     ))
