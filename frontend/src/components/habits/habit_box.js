fb465110c6f02b0ca0c40aa4c9de2028a4762b9f
Itai Farhi <itaimf@gmail.com>
2021-02-24 23:30:22 -0800

    update file tree

---
+++ frontend/src/components/habits/habit_box.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class HabitBox extends React.Component {
+  render() {
+    return (
+        <div>
+            <h3>{this.props.text}</h3>
+        </div>
+    );
+  }
+}
+
+export default HabitBox;
