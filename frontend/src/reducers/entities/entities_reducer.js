069daa051e3847fd9e0734fa0b8797a74ec5c549
James Koenig <Koenig.James@Live.com>
2021-02-28 23:57:05 -0800

    adding resources to entities reducer

--- frontend/src/reducers/entities/entities_reducer.js
+++ frontend/src/reducers/entities/entities_reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import habits from './habits_reducer';
+import resources from './resources_reducer';
 
 const EntitiesReducer = combineReducers({
   habits,
+  resources,
 });
 
 export default EntitiesReducer;
