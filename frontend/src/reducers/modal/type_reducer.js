224b8b046549976c88a8d08ab157c6112ebb56c4
James Koenig <Koenig.James@Live.com>
2021-03-18 20:31:33 -0700

    reducers now respond to the new action types

--- frontend/src/reducers/modal/type_reducer.js
+++ frontend/src/reducers/modal/type_reducer.js
@@ -1,7 +1,30 @@
+import {
+  MODAL_CLOSED,
+} from '../../actions/modal/modal_common_actions';
+import * as resource_modal_actions
+  from '../../actions/modal/resource_modal_actions';
+import * as habit_modal_actions
+  from '../../actions/modal/habit_modal_actions';
+import * as session_modal_actions
+  from '../../actions/modal/session_modal_actions';
+
+const entities_modal_actions = {
+  ...resource_modal_actions,
+  ...habit_modal_actions,
+};
+
 const type_reducer = (state="none", { type: actionType }) => {
-  switch(actionType) {
-    default:
-      return state;
+  if(actionType in entities_modal_actions) {
+    return "entities";
+  } else if(actionType in session_modal_actions) {
+    return "session";
+  } else {
+    switch(actionType) {
+      case MODAL_CLOSED:
+        return "none";
+      default:
+        return state;
+    }
   }
 }
 
