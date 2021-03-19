cd0768d840533b0a1f861672c7592c67745169ae
James Koenig <Koenig.James@Live.com>
2021-03-18 19:32:08 -0700

    start new modal actions

---
+++ frontend/src/actions/modal/modal_common_actions.js
@@ -0,0 +1,10 @@
+export const CLOSE_MODAL  = "CLOSE_MODAL";
+export const MODAL_CLOSED = "MODAL_CLOSED";
+
+export const closeModal = () => ({
+  type: CLOSE_MODAL,
+})
+
+export const modalClosed = () => ({
+  type: MODAL_CLOSED,
+});
