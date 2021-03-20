fe66f04f38bed44f829728136a8b1ed59a66d328
James Koenig <Koenig.James@Live.com>
2021-03-19 19:52:42 -0700

    removing all mentions of old modal, newModal->modal

--- frontend/src/components/modal/modal_container.js
+++ frontend/src/components/modal/modal_container.js
@@ -1,13 +1,12 @@
 import modalManifest from './modal_manifest';
 import {
-  closeModal,
   modalFadeAndClose,
 } from '../../actions/modal/modal_common_actions';
 import { connect } from 'react-redux';
 import Modal from './modal';
 
 const mSTP = ({session: { isAuthenticated},
-               ui: { newModal: { type, subtype }}}) => ({
+               ui: { modal: { type, subtype }}}) => ({
   Component: modalManifest(type, subtype),
   autoClose: type === "session" ? isAuthenticated : false,
 });
--- frontend/src/reducers/ui_reducer.js
+++ frontend/src/reducers/ui_reducer.js
@@ -1,8 +1,6 @@
 import { combineReducers } from 'redux';
-import modal from './modal/modal_reducer_old';
-import newModal from './modal/modal_reducer';
+import modal from './modal/modal_reducer';
 
 export default combineReducers({
   modal,
-  newModal,
 });
