39630a15269a848a1eee87c2d419765394669eb2
James Koenig <Koenig.James@Live.com>
2021-03-22 13:39:38 -0700

    code cleanup, fix error in new habit form

--- frontend/src/components/modal/modal.jsx
+++ frontend/src/components/modal/modal.jsx
@@ -5,7 +5,6 @@ class Modal extends React.Component {
   constructor(props) {
     super(props);
     this.handleKeyPress  = this.handleKeyPress.bind(this);
-    this.handleClick = this.handleClick.bind(this);
     this.closeModal = () => this.props.modalFadeAndClose("modal-background");
   }
 
@@ -14,32 +13,29 @@ class Modal extends React.Component {
       this.closeModal();
   }
 
-  handleClick(event) {
-    this.closeModal();
-  }
-
   componentDidMount() {
     document.addEventListener('keydown', this.handleKeyPress);
   }
 
   componentWillUnmount() {
     document.removeEventListener('keydown', this.handleKeyPress);
-    this.closeModal();
+    this.props.modalClosed();
   }
 
   render() {
     const { Component, autoClose } = this.props;
+    const { closeModal } = this;
     if(autoClose)
-      this.closeModal();
+      closeModal();
     if(!Component)
       return null;
     return (
       <div id="modal-background"
            className="modal-fade-in"
-           onClick={ this.handleClick } >
+           onClick={ closeModal } >
         <div className="modal"
              onClick={event => event.stopPropagation() } >
-          <Component closeModal={this.closeModal} />
+          <Component closeModal={closeModal} />
         </div>
       </div>
     );
