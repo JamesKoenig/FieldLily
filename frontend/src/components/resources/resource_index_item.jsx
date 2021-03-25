b7f730caba9db8e8733b20e156e3ae461df3b455
Qi Zhu <rubybfm1017@gmail.com>
2021-03-25 10:17:09 -0700

    habit show style

--- frontend/src/components/resources/resource_index_item.jsx
+++ frontend/src/components/resources/resource_index_item.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { ListGroupItem } from 'react-bootstrap';
 
 
-const ResourceIndexItem = ({ resource: { _id, title} }) => (
-  <li>
-    <Link to={`/resources/${_id}`}>{title}</Link>
-  </li>
+const ResourceIndexItem = ({ resource: { _id, title, description} }) => (
+  <ListGroupItem className="resource-item">
+    <div className="resource-desc-box">
+        <Link to={`/resources/${_id}`}>{title}</Link>
+        <p className="resource-desc">{description}</p>
+    </div>
+  </ListGroupItem>
 );
 
 export default ResourceIndexItem;
