19aaa5e5ec4be80f8e099a619353f419c81f3d5d
James Koenig <Koenig.James@Live.com>
2021-03-24 18:10:29 -0700

    seed all the seedfile users, make code modular

--- seed/seed_users.js
+++ seed/seed_users.js
@@ -5,20 +5,30 @@ const User = require("../models/User");
 
 function seedUsers() {
   let userData = loadSeedFile("user.json");
-  let user;
+  return dropUsers()
+    .then( () =>
+      Promise.all(
+        userData.map( user =>
+          seedUser(user)
+        )
+      )
+    )
+}
+
+function dropUsers() {
   return User.find().then( users=> {
+    console.log("dropping existing users collection");
     if(users.length > 0) {
-      console.log("dropping existing users collection");
       return User.collection.drop();
     } else {
       console.log("empty users db, no need to drop");
       return Promise.resolve();
     } })
-    .then( () => {
-      console.log("seeding user");
-      user = userData[0]; })
-    .then(() =>
-      bcrypt.genSalt(10))
+}
+
+function seedUser(user) {
+  console.log(`seeding user: ${user.email}`);
+  return bcrypt.genSalt(10)
     .then( salt =>
       bcrypt.hash(user.password, salt))
     .then( hash =>
