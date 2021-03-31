88e17290d42e353c8ead7c4e30329b9d1de1684e
James Koenig <Koenig.James@Live.com>
2021-03-30 20:10:28 -0700

    remove placeholder text froim main page

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Resources from '../resources/resources_container';
 import './main.css'
 
 class MainPage extends React.Component {
@@ -7,8 +6,6 @@ class MainPage extends React.Component {
   render() {
     return (
       <div className="main-page">
-        <h1>Field Lily</h1>
-        <Resources />
       </div>
     );
   }
