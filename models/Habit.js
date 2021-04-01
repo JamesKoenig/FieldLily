068d5d7f7fb3a7a5a0b2db8f38fc4a93b8e114ee
James Koenig <Koenig.James@Live.com>
2021-04-01 16:12:20 -0700

    add timestamps, change indentation

--- models/Habit.js
+++ models/Habit.js
@@ -2,18 +2,21 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const HabitSchema = new Schema({
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: 'User'
-  },
-  title: {
-    type: String,
-    required: true
-  },
-  description: {
-    type: String,
-    required: true
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    }
+  }, {
+    timestamps: true,
   }
-});
+);
 
 module.exports = Habit = mongoose.model('Habit', HabitSchema);
