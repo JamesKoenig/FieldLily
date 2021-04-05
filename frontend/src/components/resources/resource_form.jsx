ab91d788d1884396b0b564e029ffa3e4a38564ba
James Koenig <Koenig.James@Live.com>
2021-04-04 22:31:27 -0700

    adding both title and desc to form, store

--- frontend/src/components/resources/resource_form.jsx
+++ frontend/src/components/resources/resource_form.jsx
@@ -3,8 +3,12 @@ import React from 'react';
 class ResourceForm extends React.Component {
     constructor(props) {
         super(props);
+        this.state = { heading: "" };
         if(this.props.resource) {
+          this.state.heading = "Edit your resource";
           this.props.receiveNewResource(this.props.resource);
+        } else {
+          this.state.heading = "Create new resource";
         }
         this.handleSubmit = this.handleSubmit.bind(this)
         this.update = this.update.bind(this)
@@ -26,24 +30,35 @@ class ResourceForm extends React.Component {
           receiveNewResource({
             title,
             description,
-            [field]: e.currentTarget.value
+            [field]: e.currentTarget.value,
           });
         }
     }
 
     render() {
+      const {
+        title,
+        description,
+      } = this.props;
+      const { heading } = this.state;
         return (
             <div>
-                <h3>{this.props.formType}</h3>
+                <h3>{heading}</h3>
                 <form onSubmit={this.handleSubmit}>
 
                   <label>
                       Title
                   </label>
-                  <textarea
-                    value={this.props.title}
+                  <input
+                    type="text"
+                    value={title}
                     onChange={this.update('title')} />
-
+                  <label>
+                    Description
+                  </label>
+                  <textarea
+                    value={description}
+                    onChange={this.update('description')} />
                   <button>
                     add Resource
                   </button>
