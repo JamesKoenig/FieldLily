9e86875c37d88514681e73166fad7c2c1bd279a1
James Koenig <Koenig.James@Live.com>
2021-04-15 02:00:34 -0700

    basic prompt outline

--- frontend/src/components/habits/habit_delete_confirmation.jsx
+++ frontend/src/components/habits/habit_delete_confirmation.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import { connect } from 'react-redux'
 
-const HabitDeleteConfirmPrompt = () => {
+import './habit_delete_confirmation.css';
+
+const mSTP = state => ({
+  habit: state.entities.habits.all[state.ui.modal.misc.habitId],
+})
+
+const HabitDeleteConfirmPrompt = ({habit}) => {
   return (
-    <p>hello world!</p>
+    <div id="habit-delete-confirmation">
+      <h2>are you sure you want to delete {habit.title}?</h2>
+      <h1 className="confirmation-warning">this cannot be undone</h1>
+      <p>
+        if you still want to continue, please enter the habit title below
+      </p>
+      <form>
+        <input />
+        <button onClick={() => {}}>
+        </button>
+      </form>
+    </div>
   );
 }
 
-export default HabitDeleteConfirmPrompt;
+export default connect(
+    mSTP,
+  )(
+    HabitDeleteConfirmPrompt
+  );
