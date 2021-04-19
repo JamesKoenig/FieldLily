408090ad28dd5cf5f9f4edba3f0d9f9ed88ae331
James Koenig <Koenig.James@Live.com>
2021-04-19 14:48:38 -0700

    abstract confirmation prompt to keep DRY

--- frontend/src/components/habits/habit_delete_confirmation.jsx
+++ frontend/src/components/habits/habit_delete_confirmation.jsx
@@ -1,66 +1,47 @@
-import React, {
-  useState,
-} from 'react';
 import { connect } from 'react-redux'
-import { useHistory } from 'react-router-dom';
-
 import { destroyHabit } from "../../actions/habit_actions";
-import { modalFadeAndClose } from "../../actions/modal/modal_common_actions";
 
-import './habit_delete_confirmation.css';
+import entityDeleteConfirmPrompt from '../modal/confirm_delete';
 
-const mSTP = state => ({
-  habit: state.entities.habits.all[state.ui.modal.misc.habitId],
-})
+const mSTP = ({
+  entities: {
+    habits: {
+      all: habits,
+    }
+  },
+  ui: {
+    modal: {
+      misc: {
+        habitId,
+      }
+    }
+  },
+})=> {
+  const { _id: id, title } = habits[habitId] ||
+     { _id: undefined, title: undefined };
+  return {
+    type: "habit",
+    id,
+    title,
+  }
+}
 
 const mDTP = {
-  destroyHabit,
-  modalFadeAndClose,
+  deleteEntityCallback: destroyHabit,
 }
 
-const HabitDeleteConfirmPrompt = ({
-  habit,
-  destroyHabit,
-  modalFadeAndClose
-}) => {
-  const [ titleInput, setTitleInput ] = useState('');
-  const history = useHistory();
-  const disabled = (titleInput !== habit.title);
-
-  const handleChange = event => {
-    setTitleInput(event.target.value);
-  }
-
-  const handleSubmit = event => {
-    event.preventDefault();
-    if(!disabled) {
-      destroyHabit(habit._id);
-      history.goBack();
-      modalFadeAndClose();
-    }
-  }
-
-  return (
-    <div id="habit-delete-confirmation">
-      <h2>are you sure you want to delete {habit.title}?</h2>
-      <h1 className="confirmation-warning">this cannot be undone</h1>
-      <p>
-        if you still want to continue, please enter the habit title below
-      </p>
-      <form onSubmit={handleSubmit}>
-        <input value={titleInput}
-               onChange={handleChange} />
-        <button {...disabled}>
-          DELETE
-        </button>
-      </form>
-    </div>
-  );
-}
+const mergeProps = (stateProps, dispatchProps, ownProps) => ({
+  ...stateProps,
+  ...dispatchProps,
+  ...ownProps,
+  deleteEntityCallback: () =>
+    dispatchProps.deleteEntityCallback(stateProps.id),
+});
 
 export default connect(
     mSTP,
     mDTP,
+    mergeProps,
   )(
-    HabitDeleteConfirmPrompt
+    entityDeleteConfirmPrompt
   );
---
+++ frontend/src/components/modal/confirm_delete.jsx
@@ -0,0 +1,47 @@
+import React, {
+  useState,
+} from 'react';
+import { useHistory } from 'react-router-dom';
+
+import './confirm_delete.css';
+
+const entityDeleteConfirmPrompt = ({
+  type,
+  title,
+  deleteEntityCallback,
+}) => {
+  const [ titleInput, setTitleInput ] = useState('');
+  const history = useHistory();
+  const disabled = (titleInput !== title);
+
+  const handleChange = event => {
+    setTitleInput(event.target.value);
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    if(!disabled) {
+      deleteEntityCallback();
+      history.goBack();
+    }
+  }
+
+  return (
+    <div id="entity-delete-confirmation">
+      <h2>are you sure you want to delete {title}?</h2>
+      <h1 className="confirmation-warning">this cannot be undone</h1>
+      <p>
+        if you still want to continue, please enter the {type} title below
+      </p>
+      <form onSubmit={handleSubmit}>
+        <input value={titleInput}
+               onChange={handleChange} />
+        <button {...disabled}>
+          DELETE
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default entityDeleteConfirmPrompt;
