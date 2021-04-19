772ac92df2353016cac5199db2fbc561db85efb7
Itai Farhi <itaimf@gmail.com>
2021-04-19 16:17:11 -0700

    add conditional statement to logger

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,14 +1,18 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
-
 import rootReducer from '../reducers/root_reducer';
+
+let middleware = [thunk]
+if (process.env.NODE_ENV !== 'production') {
+  middleware = [...middleware, logger]
+}
 
 const configureStore = (preloadedState = {}) => (
   createStore(
     rootReducer,
     preloadedState,
-    applyMiddleware(thunk, logger)
+    applyMiddleware(...middleware)
   )
 );
 
