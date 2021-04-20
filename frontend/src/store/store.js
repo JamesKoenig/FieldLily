4875a3d85d0472735539b2fe4377c55d91dd8663
James Koenig <viscous.cycle@gmail.com>
2021-04-20 16:31:20 -0700

    Revert "Itai's Redux Logger as dev-dependency changes"

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,18 +1,14 @@
-import { createStore, applyMiddleware} from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
-import rootReducer from '../reducers/root_reducer';
 
-let middleware = [thunk]
-if (process.env.NODE_ENV !== 'production') {
-  middleware = [...middleware, logger]
-}
+import rootReducer from '../reducers/root_reducer';
 
 const configureStore = (preloadedState = {}) => (
   createStore(
     rootReducer,
     preloadedState,
-    applyMiddleware(...middleware)
+    applyMiddleware(thunk, logger)
   )
 );
 
