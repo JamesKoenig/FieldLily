8e7e2ef05b9e4aba9395ab32e33d82d3f2bb86f5
Itai Farhi <itaimf@gmail.com>
2021-04-21 16:32:20 -0700

    draft of confirm_logout

--- frontend/src/components/modal/confirm_logout.jsx
+++ frontend/src/components/modal/confirm_logout.jsx
@@ -1,15 +1,29 @@
-return (
-    <div id="logout-confirmation">
-      <h2>are you sure you want to logout?</h2>
-      <p>
-        if you still want to continue, please click the button
-      </p>
-      <form onSubmit={handleSubmit}>
-        <input value={titleInput}
-               onChange={handleChange} />
-        <button disabled={disabled} >
-          DELETE
+import React, {useState} from 'react';
+import './confirm_delete.css';
+
+const logoutConfirmPrompt = ({
+    user,
+    closeModal
+  }) => {
+    const [ titleInput, setTitleInput ] = useState('');
+  
+    const handleSubmit = event => {
+      event.preventDefault();
+      
+      closeModal();
+      }
+  
+    return (
+        <div id="logout-confirmation">
+        <h2>are you sure you want to logout {user}?</h2>
+        <p>
+            if you still want to continue, please click the button
+        </p>
+        <button onClick={handleSubmit} >
+            LOGOUT
         </button>
-      </form>
-    </div>
-  );
+        </div>
+    );
+}
+
+export default logoutConfirmPrompt
