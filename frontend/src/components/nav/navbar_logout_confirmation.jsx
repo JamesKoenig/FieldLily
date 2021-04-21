19511e89b7101970dcafb7e013c5de6eb29ac55f
Itai Farhi <itaimf@gmail.com>
2021-04-21 15:58:53 -0700

    use habit delete as model

---
+++ frontend/src/components/nav/navbar_logout_confirmation.jsx
@@ -0,0 +1,50 @@
+import { connect } from 'react-redux'
+import { closeModal } from '../../actions/modal/modal_common_actions';
+import { destroyHabit } from "../../actions/habit_actions";
+
+import entityDeleteConfirmPrompt from '../modal/confirm_delete';
+
+const mSTP = ({
+  entities: {
+    habits: {
+      all: habits,
+    }
+  },
+  ui: {
+    modal: {
+      misc: {
+        habitId,
+      }
+
+    }
+  },
+}) => {
+  const { _id: id, title } = habits[habitId] ||
+     { _id: undefined, title: undefined };
+  return {
+    type: "habit",
+    id,
+    title,
+  }
+}
+
+const mDTP = {
+  deleteEntityCallback: destroyHabit,
+  closeModal,
+}
+
+const mergeProps = (stateProps, dispatchProps, ownProps) => ({
+  ...stateProps,
+  ...dispatchProps,
+  ...ownProps,
+  deleteEntityCallback: () =>
+    dispatchProps.deleteEntityCallback(stateProps.id),
+});
+
+export default connect(
+    mSTP,
+    mDTP,
+    mergeProps,
+  )(
+    entityDeleteConfirmPrompt
+  );
